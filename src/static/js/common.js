001289d958f086e95eb7e85806e15d63277ed841
Aslan <berezg86@gmail.com>
2020-12-12 20:52:20 +0300

    Сложные табы

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -6,5 +6,37 @@ $(document).ready(function () {
 
     $('.selectcustom select').selectric()
 
+    var screenWidth = $(window).width();
+
+    if (screenWidth < 700) {
+        $('.tabbuttons li').click(function () {
+            var tab = $(this).attr('data-tab');
+            $('.tabcontent__item').not(tab).css({ 'display': 'none' });
+            $('.tabcontent').fadeIn()
+            if (tab == '#tab2') {
+                $('.tabcontent').addClass('wheat')
+            }
+            if (tab == '#tab3') {
+                $('.tabcontent').addClass('green')
+            }
+            $(tab).fadeIn(400);
+
+        });
+        $('.tabbuttons li:first').click();
+        $('.closetab').click(function () {
+            $('.tabcontent').removeClass('green').removeClass('wheat')
+            $('.tabcontent__item').fadeOut()
+            $('.tabcontent').fadeOut()
+        })
+    } else {
+        $('.tabbuttons li').click(function () {
+            $('.tabbuttons li').removeClass('is-active');
+            $(this).addClass('is-active');
+            var tab = $(this).attr('data-tab');
+            $('.tabcontent__item').not(tab).css({ 'display': 'none' });
+            $(tab).fadeIn(400);
+        });
+        $('.tabbuttons li:first').click();
+    }
 });
 

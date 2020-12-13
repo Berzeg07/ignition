bc411af14075c28c9d7acccd6e0b6b569778341a
Aslan <berezg86@gmail.com>
2020-12-13 10:56:16 +0300

    Верстка главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -37,5 +37,105 @@ $(document).ready(function () {
         });
         $('.tabbuttons li:first').click();
     }
+
+    var markSlider = new Swiper('.mark-slider', {
+        slidesPerView: 5,
+        spaceBetween: 24,
+        loop: true,
+        observer: true,
+        observeParents: true,
+        navigation: {
+            nextEl: '.mark-slider__next',
+            prevEl: '.mark-slider__prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1.2,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 1.6,
+                spaceBetween: 20,
+            },
+            1279: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1599: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            }
+        }
+    });
+
+    var myAdvSlider = undefined;
+
+    function initSwiper() {
+        var screenWidth = $(window).width();
+        if (screenWidth > 1279 && myAdvSlider == undefined) {
+            var advSlider = new Swiper('.team-slider', {
+                slidesPerView: 4,
+                spaceBetween: 20,
+                loop: true,
+                navigation: {
+                    nextEl: '.team-slider__next',
+                    prevEl: '.team-slider__prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+
+                breakpoints: {
+                    1599: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                }
+            });
+        } else if (screenWidth < 1280 && myAdvSlider != undefined) {
+            myAdvSlider.destroy();
+            myAdvSlider = undefined;
+        }
+    }
+
+    //Swiper plugin initialization
+    initSwiper();
+
+    ymaps.ready(init);
+
+    function init() {
+        var center = [55.59113656911934, 37.88662649999996];
+        var myMap = new ymaps.Map('map', {
+            center: center,
+            controls: [],
+            zoom: 7
+        }, {
+            searchControlProvider: 'yandex#search'
+
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+        var myPlacemark = new ymaps.Placemark(center, {
+            // Свойства.
+            // Содержимое иконки, балуна и хинта.
+            balloonContent: 'улица Ивана Франко, 4к4',
+            hintContent: 'улица Ивана Франко, 4к4'
+        }, {
+            // Опции.
+            iconLayout: 'default#image',
+            // iconImageHref: 'img/map-ic.png',
+            // iconImageSize: [42, 42]
+            // preset: 'twirl#violetIcon'
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+
 });
 
